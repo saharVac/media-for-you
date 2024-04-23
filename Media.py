620c6179c157418de56cc57b9a9eff5e0067b299
saharv-av <vcsahar@gmail.com>
2024-04-23 12:56:36 -0600

    Wrote functionality of Media class

--- Media.py
+++ Media.py
@@ -2,11 +2,26 @@
 # Date created: 4/23/24
 # Description: Media class
 
-# Member variables to store an ID, a title, and an average rating
+class Media:
 
+    # Member variables to store an ID, a title, and an average rating
+    _id = ""
+    _title = ""
+    _avgRating = 0
 
-# constructor that takes in an ID, a title, and an average rating as parameters and
-# assigns those values to the appropriate member variables
+    # constructor that takes in an ID, a title, and an average rating as parameters and assigns those values to the appropriate member variables
+    def __init__(self, id, title, avgRating):
+        self._id = id
+        self._title = title
+        self._avgRating = avgRating
 
+    # accessor/mutator functions
+    def getId(self):
+        return self._id
 
-# accessor/mutator functions
+    def getTitle(self):
+        return self._title
+
+    def getAvgRating(self):
+        return self._avgRating
+
